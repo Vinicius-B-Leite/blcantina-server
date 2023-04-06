000136c2ead01a472fce70ab0715e94fbee9452e
Vinicius B. Leite1 <viniciusbleite21@gmail.com>
2023-04-06 11:44:32 -0300

    feature: criando e buscando produtos

---
+++ src/controller/product/index.ts
@@ -0,0 +1,39 @@
+import { Request, Response } from "express";
+import { prisma } from "../../prisma";
+
+class ProductController {
+    async store(req: Request, res: Response) {
+        const {
+            imageURL,
+            name,
+            price,
+            categoryID,
+            priceRebate, } = req.body
+
+        const product = await prisma.product.create({
+            data: {
+                imageURL,
+                name,
+                price,
+                categoryID,
+                priceRebate,
+            }
+        })
+
+        return res.json(product)
+    }
+
+    async index(req: Request, res: Response) {
+        const products = await prisma.category.findMany({
+            include: {
+                product: true
+            }
+        })
+
+
+        return res.json(products)
+    }
+
+}
+
+export const productController = new ProductController()
--- src/routes.ts
+++ src/routes.ts
@@ -1,10 +1,14 @@
 import { Router } from "express";
 import { orderController } from "./controller/order";
 import { categoryController } from "./controller/category";
+import { productController } from "./controller/product";
 
 const routes = Router()
 
 routes.get('/order', orderController.show)
+
+routes.post('/product', productController.store)
+routes.get('/product', productController.index)
 
 routes.post('/category', categoryController.store)
 routes.get('/category', categoryController.index)
